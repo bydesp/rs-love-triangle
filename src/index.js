2d876f0abf7335c2c6207dfb0e460517cc441229
Anton Dvornikov <anton@MacBook-Pro-Anton.local>
2019-02-20 12:31:07 +0300

    done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(lovers = []) {
+   if (lovers.length < 3) return 0
+   let counter = 0
+
+   lovers.forEach((value, index) => {
+     const firstLover = index
+     const secondLover = value - 1
+     const thirdLover = lovers[secondLover] - 1
+
+     if (lovers[firstLover] === undefined || lovers[secondLover] === undefined || lovers[thirdLover] == undefined) return
+     if (firstLover === secondLover || firstLover === thirdLover || secondLover === thirdLover) return
+
+     if (lovers[thirdLover] - 1 === firstLover) {
+       lovers[firstLover] = lovers[secondLover] = lovers[thirdLover] = undefined
+       counter++;
+     }
+   })
+
+   return counter
 };
